ce8bb7d526929a190aa0a5837c7ec8fd5570cef9
Bryce Dunn <bryce.cc.dunn@gmail.com>
2021-04-04 21:00:24 -0500

    initial commit

---
+++ let_and_const_exercise.js
@@ -0,0 +1,21 @@
+//old version
+// var PI = 3.14;
+// PI = 42;
+
+// ES2015
+const PI = 3.14;
+PI = 42 //will ont work
+
+/*
+Quiz
+
+1. variables declared with var can be redeclared, variables declared with let cannot be. Furthermore, var scopes to the window, let is block-scoped.
+
+2. variables declared with const cannot be reassigned or redeclared. with var they can. Same scope as above.
+
+3. variables declared with let can be reassigned, with const they cannot.
+
+4. hoisting is essentially pulling variable delcarations to the 'top' of an application without fully defining them. Therefore they can be accessed (as undefined) before their full initialization in the code. (only with var)
+
+
+*/
